8aec5606b0a446bee934649dfc8edafae99a9604
Emanuel.ps <emanuelpages.ps@gmail.com>
2022-06-20 19:01:22 -0300

    subida 1: subuida completa - header y navbar

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className='header'>
+        <p> Envío gratis en compras superiores a $5.999 </p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <nav className='navbar'>
+        <button>Inicio</button>
+        <button>Productos</button>
+        <button>Nosotros</button>
+        <button>Contacto</button>
+    </nav>
+  );
+}
+
+export default NavBar;
